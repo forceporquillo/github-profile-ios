5cbe7befb809232ccf5d270dccd847540272c242
Aljan Porquillo <fporquillo18@gmail.com>
2024-08-18 17:46:26 +0800

    [chore] Initial commit

---
+++ gitprofile/gitprofileApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitprofileApp.swift
+//  gitprofile
+//
+//  Created by Aljan Porquillo on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct gitprofileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
